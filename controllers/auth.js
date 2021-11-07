44dedfc70aef76c2cef99430e1103d2aab27ba09
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-07 20:11:11 -0300

    Importar Jsonwebtoken e criar um Token para retornar na resposta do servidor.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
 // controllers
-const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
+
 
 
 // ATTENTION: never repass passwords AS Strings!!! 
@@ -15,7 +17,9 @@ const register = async (req, res) => {
 	
 	const user = await User.create({ ...req.body });
 	
-	res.status(StatusCodes.CREATED).json({ user });
+	const token = jwt.sign({ userId: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d'} );
+	
+	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 
