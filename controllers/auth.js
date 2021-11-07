2af0d1eb5b0b70c483ba9d07e4ca444ec33c1686
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-07 18:34:38 -0300

    Inserir banco de dados para a rota register-auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,13 @@
 // controllers
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
+
 
 const register = async (req, res) => {
+	const user = await User.create({ ...req.body });
 	
-	res.send('register user');
+	res.status(StatusCodes.CREATED).json({ user });
 };
 
 
