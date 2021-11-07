079bcb31d639a6070a7c32268a40589c5dab9e7a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-07 18:43:11 -0300

    Testar validacao de erro manual, sem o mongoose, mas usando os erros customizados

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,18 @@
 // controllers
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 
 
 const register = async (req, res) => {
+	// Manual validation without Mongoose
+	// const { name, email, password } = req.body;
+	
+	// if (!name || !email || !password) {
+		// throw new BadRequestError('Please, provide name, email and password');
+	// }
+	
 	const user = await User.create({ ...req.body });
 	
 	res.status(StatusCodes.CREATED).json({ user });
