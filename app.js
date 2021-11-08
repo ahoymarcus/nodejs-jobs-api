25c838e60b2302ba234f72eecb6044e466482270
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-08 11:58:30 -0300

    Refatorar Job Schema e tambem inserir validacoes do mongoose

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=qwfE7fSVaZM
-// 8 hrs  00' 00''
+// 8 hrs  13' 00''
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
--- models/Job.js
+++ models/Job.js
@@ -2,18 +2,34 @@
 const mongoose = require('mongoose');
 
 
-const JobSchema = mongoose.Schema({
-	name: String,
-	status: String
-});
+const JobSchema = new mongoose.Schema({
+	company: {
+		type: String,
+		required: [true, 'Please, provide a company name'],
+		maxlength: 50
+	},
+	position: {
+		type: String,
+		required: [true, 'Please, provide a position'],
+		maxlength: 100
+	},
+	status: {
+		type: String,
+		enum: ['interview', 'declined', 'pending'],
+		default: 'pending'
+	},
+	createdBy: {
+		type: mongoose.Types.ObjectId,
+		ref: 'User',
+		required: [true, 'Please, provide a user']
+	}
+}, { timestamps: true });
+
+
 
 
 
 module.exports = mongoose.model('Job', JobSchema);
-
-
-
-
 
 
 
