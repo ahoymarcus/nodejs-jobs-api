34c89597a15848b02bcfb3b3fc40c46314c5e325
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-08 19:32:35 -0300

    Refatorar middleware error-handler.js para retornar msgs do mongoose mais amigaveis

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,12 +4,34 @@ const { StatusCodes } = require('http-status-codes')
 
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
+  console.log(err);
+	
+	let customError = {
+		// set default
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong, try again later'
+	};
+	
+	if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  
+	// Tornar devolução de erros do Mongoose mais
+	// user friedly
+	if (err.code && err.code === 11000) {
+		customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field. Please, choose another value`;
+		customError.statusCode = 400;
+	}
+	
+	
+	// return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+	return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 
+
+
 module.exports = errorHandlerMiddleware
+
+
 
 
