4d6470f30894e8d2874b76aa84330e05f826a1bf
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-08 19:59:14 -0300

    Instalar pacotes de seguranca em app.js e configuracoes diversas: Helmet, Cors, Xss-clean, Express-rate-limit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // https://www.youtube.com/watch?v=qwfE7fSVaZM
-// 9 hrs  10' 00''
+// 9 hrs  13' 00''
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
@@ -19,9 +25,23 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const app = express();
 
-app.use(express.json());
-// extra packages
 
+// extra package
+app.set('trust proxy', 1); // config for Heroku's apps
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // limit each IP to 100 requests per windowMs
+	})
+);
+
+// basic middleware
+app.use(express.json());
+
+// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 // routes
